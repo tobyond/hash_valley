102863779856c05a869051521160beef74374f7f
Toby <toby.holmes@interfolio.com>
2020-09-20 15:40:45 -0700

    cleaning up paste mess

--- lib/hash_valley/hash_validator.rb
+++ lib/hash_valley/hash_validator.rb
@@ -38,23 +38,3 @@ class HashValidator
     end
   end
 end
-   hash = {
-     first_name: "",
-     last_name: "Last",
-     email: "first@last#error_email.com"
-   }
-   validations = {
-     first_name: { presence: true },
-     last_name: { inclusion: { in: ["Last", "Second Last"] }, allow_blank: true },
-     email: { format: { with: URI::MailTo::EMAIL_REGEXP } }
-   }
-   validator = HashValidator.new(hash, validations)
-   validator.valid?
-   => false
-   validator.errors.messages
-   => {:first_name=>["can't be blank"], :email=>["is invalid"]}
-  
-   for a list of the validations available:
-   https://guides.rubyonrails.org/active_record_validations.html
-  
-   if you don't send in a validations hash then it will default to presence: true
